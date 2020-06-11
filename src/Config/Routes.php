1b7d124fde6f929046c5f1a0c367fb771c07ce8d
Loru Fabrice <fabrice@adnduweb.com>
2020-06-11 14:33:20 +0200

    Update Routes.php

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -15,6 +15,20 @@
         $routes->get('(:any)/catalogue/category/add', 'AdminCategoryController::renderForm');
         $routes->post('(:any)/catalogue/category/add', 'AdminCategoryController::postProcess');
         $routes->get('(:any)/catalogue/category/dupliquer/(:segment)', 'AdminCategoryController::dupliquer/$2');
-        // $routes->get('public/blog/settings', 'AdminBlogSettingsController::renderForm');
-        // $routes->post('public/blog/settings', 'AdminBlogSettingsController::postProcess');
     });
+
+
+
+    $locale = '/';
+    if (service('Settings')->setting_activer_multilangue == true) {
+        $locale = '/{locale}';
+    }
+
+    //E-commerce
+    $routes->get($locale . '/logout', 'FrontAuthenticationController::logout', ['namespace' => '\Adnduweb\Ci4_ecommerce\Controllers\Front', 'as' => 'logout-customer']);
+    $routes->get($locale . '/signin', 'FrontAuthenticationController::SignIn', ['namespace' => '\Adnduweb\Ci4_ecommerce\Controllers\Front', 'as' => 'signin']);
+    $routes->post($locale . '/signin', 'FrontAuthenticationController::postProcessSignIn', ['namespace' => '\Adnduweb\Ci4_ecommerce\Controllers\Front']);
+    $routes->get($locale . '/signup', 'FrontAuthenticationController::SignUp', ['namespace' => '\Adnduweb\Ci4_ecommerce\Controllers\Front']);
+    $routes->post($locale . '/signup', 'FrontAuthenticationController::postProcessSignUp', ['namespace' => '\Adnduweb\Ci4_ecommerce\Controllers\Front']);
+    $routes->get($locale . '/activate-account-customer', 'FrontAuthenticationController::ActivateAccount', ['namespace' => '\Adnduweb\Ci4_ecommerce\Controllers\Front']);
+    $routes->get($locale . '/my-account', 'FrontAccountController::index', ['namespace' => '\Adnduweb\Ci4_ecommerce\Controllers\Front', 'filter' => 'loginCustomer']);
